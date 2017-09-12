965ac6a54ce9f370b912875df81f3e7a692afbd2
wangj <810112917@qq.com>
2017-09-12 08:35:33 +0000

    优化 优惠头条优化原因删除按钮显示

--- resources/views/admin/ks/dhor/index.blade.php
+++ resources/views/admin/ks/dhor/index.blade.php
@@ -47,7 +47,7 @@
                                             <a class=" op_edit"  href="{{route('admin.ks.dhor.edit',$info->r_id)}}"
                                                style="margin-right: 10px;display: none">
                                                 <i class="fa fa-pencil-square-o " aria-hidden="true">修改</i></a>
-                                            @if($k > 1)
+                                            @if($info->type != 1)
                                             <a style="display: none"  class=" op_destroy"  href="javascript:del('{{route('admin.ks.dhor.destroy',$info->r_id)}}')">
                                                 <i class="fa  fa-trash-o " aria-hidden="true">删除</i></a>
                                             @endif
