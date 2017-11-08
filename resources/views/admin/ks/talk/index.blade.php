77810fa378b8a96a258292a174c946136baf6373
whf <306623777@qq.com>
2017-11-08 20:31:00 +0800

    对我说模块

--- resources/views/admin/ks/talk/index.blade.php
+++ resources/views/admin/ks/talk/index.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-xs-12">
                 <div class="box">
-
+					<iframe src="" width="" height="" name="MyIFrame" id="MyIFrame"></iframe>
 
                     <!--box-footer-->
                 </div>
@@ -15,10 +15,60 @@
     </section>
 @endsection
 
-@section('css')
-
-@endsection
 
 @section('js')
+<script src="/talkMe/js/jquery.cookie.js" type="text/javascript" charset="utf-8"></script>
+<script src="/talkMe/webdemo/im/js/util.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript">
+	
+	 $(function(){
+	 	 $.ajaxSetup({
+            layerIndex:-1,
+            beforeSend: function () {
+                
+            },
+            complete: function () {
+               
+            },
+            error: function () {
+                layer.alert('部分数据加载失败，可能会导致页面显示异常，请刷新后重试', {
+                    skin: 'layui-layer-molv'
+                    , closeBtn: 0
+                    , shift: 4 //动画类型
+                });
+            }
+        });
+	 	function getIframeContent(){  //获取iframe中文档内容
+			 var doc;
+			 if (document.all){ // IE 
+			  doc = document.frames["MyIFrame"].document; 
+			 }else{ // 标准
+			  doc = document.getElementById("MyIFrame").contentDocument; 
+			 }
+			 return doc;
+		} 
+		var win=getIframeContent()
+		if(!readCookie("laravel_session")){
+			win.location.href ="{{route('admin.login')}}"; 
+		}else{
+			$.get("/talk/getLoginInfo",function(res){
+				console.log(res)
+				$.cookie('uid', res.account, { expires: 7, path: '/' }); 
+				$.cookie('sdktoken', res.token, { expires: 7, path: '/' }); 
+				win.location.href ="/talkMe/webdemo/im/main.html"; 
 
+			})
+		}
+
+  	})
+</script>
+@endsection
+
+@section('css')
+	<style>
+		#MyIFrame{
+			width: 100%;
+			height: 900px;
+		}
+	</style>
 @endsection
